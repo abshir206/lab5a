cb0feb6a0054f1d894b6eb7f6501867da358e83d
Abshir Ibrahim <abshir.ibrahim@outlook.com>
2023-02-06 22:13:00 -0800

    lab5a

--- js/app.js
+++ js/app.js
@@ -9,11 +9,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
+  let answer= a+b;
+  return [answer, `The sum of ${a} and ${b} is ${answer}.`];
+
 
 }
 
+
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+ testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -27,11 +31,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+  let answer = a*b;
+  return [answer, `The product of ${a} and ${b} is ${answer}.`];
+
 
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -48,6 +55,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let answer =
 
 }
 
